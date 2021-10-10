7272ca468c3d629719cc401ffaf1376e40e937a0
Mozdir Taras <letsokey@mail.ru>
2021-10-10 13:00:02 +0400

    Created Addapter Calculator

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -2,12 +2,12 @@ package org.example;
 
 /**
  * Мэин класс
- *
  */
-public class MainClass
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MainClass {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.mult(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
     }
 }
